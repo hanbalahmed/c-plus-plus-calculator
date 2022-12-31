da22f6135d1d3e0f7a7ed51f588088670b117eef
1b7t <72215552+hanbalahmed@users.noreply.github.com>
2022-12-31 21:20:47 +0500

    addition code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2,sum; // makes the variables
+
+    // Addition
+    cout << "Enter two numbers" << endl; // takes first number
+    cin >> num1 >> num2; // takes an input. equals to input(); in python
+    sum = num1 + num2; // the sum will equal the first number add second number
+    cout << num1 << "+" << num2 << "=" << sum << endl; // does number1 + number2 = sum
+
+    system("pause"); // enables "press key to close"
+    return 0;
+}
