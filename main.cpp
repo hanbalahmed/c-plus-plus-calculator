1839438e344f8705212aeea24f4b8d7df5d09163
1b7t <72215552+hanbalahmed@users.noreply.github.com>
2022-12-31 21:26:40 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     int num1,num2,sum; // makes the variables
 
     // Addition
-    cout << "Enter two numbers" << endl; // takes first number
+    cout << "Enter two numbers" << endl; // takes first number and secon number
     cin >> num1 >> num2; // takes an input. equals to input(); in python
     sum = num1 + num2; // the sum will equal the first number add second number
     cout << num1 << "+" << num2 << "=" << sum << endl; // does number1 + number2 = sum
